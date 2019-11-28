fdac8c8c57bb058778df83431f469440ebfefb2e
adrian19lb <adrian.ademianiuk.pl>
2019-11-28 23:33:18 +0100

    Refactor LinearFunction object

--- src/main/webapp/resources/js/linearFunction.js
+++ src/main/webapp/resources/js/linearFunction.js
@@ -1,24 +1,34 @@
 function LinearFunction(begin , end) {
     this.a = (begin.y - end.y)/(begin.x - end.x);
     this.b = (begin.y - this.a * begin.x);
+    competeFunctionFormula.call(this, begin);
+    formulaTrimmer.call(this);
+}
+
+function competeFunctionFormula(node) {
+    if(isInfinity(this.a)) {
+       this.functionFormula = ['x', ' = ', node.x]; 
+    }else {
+        this.functionFormula = ['y', ' = ', this.a, 'x', ' + ', this.b];
+        this._trim = { 'a' : [2, 3], 'b' : [0, -2] }; //prepare trim object if a or b doesn't exist
+    }
+}
+
+function isInfinity(value) {
+    return value === Infinity || value === -Infinity;
+}
+
+function formulaTrimmer() {
+    const DOES_NOT_EXIST = 0;
+    if (this.b === DOES_NOT_EXIST) {
+        this.functionFormula = Array.prototype.slice.apply(this.functionFormula, this._trim.b);
+    }else if (this.a === DOES_NOT_EXIST) {
+        Array.prototype.splice.apply(this.functionFormula, this._trim.a);
+    }
 }
 
 LinearFunction.prototype.getFormula = function() {
-    let functionFormula = ['y', ' = ', this.a, 'x', ' + ', this.b];
-    let formula = formulaSlicer(functionFormula);
-
-    return formula.reduce((accumulator, currentValue) => accumulator + currentValue); 
-}
-
-function formulaSlicer(mathFunction) {
-    const NULL = 0;
-    if (mathFunction[mathFunction.length - 1] === NULL) {
-        mathFunction = mathFunction.slice(0, -2);
-    }else if (mathFunction[2] == NULL) {
-        mathFunction.splice(2, 3);
-    }
-
-    return mathFunction;
+    return this.functionFormula.reduce((accumulator, currentValue) => accumulator + currentValue); 
 }
 
 exports.LinearFunction = LinearFunction;
