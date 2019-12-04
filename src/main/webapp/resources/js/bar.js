39274e63e01f34112d06690769aa440ff7b58b51
adrian19lb <adrian.ademianiuk.pl>
2019-12-04 23:00:03 +0100

    Add this.toVector2 property

--- src/main/webapp/resources/js/bar.js
+++ src/main/webapp/resources/js/bar.js
@@ -4,6 +4,7 @@ var LinearFunction = require('./linearFunction.js').LinearFunction;
 function Bar(begin, end) {
     this.begin = begin;
     this.end = end;
+    this.toVector2 = end.position.subtract(begin.position);
     this.mathFunction = new LinearFunction(begin.position, end.position);
     Object.freeze(this.mathFunction);
     Object.seal(this.mathFunction);
